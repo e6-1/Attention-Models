6125261c2643c9f1e48202e50c0d0b8cf293517a
Ethan Petersen <ethanpetersen@ethans-mbp.wlan.rose-hulman.edu>
2017-06-06 12:21:18 -0400

    Initial commit for DRAM implementation.

--- utils.py
+++ utils.py
@@ -7,6 +7,13 @@ import tensorflow as tf
 distributions = tf.contrib.distributions
 
 
+def conv2d(x, W, b, strides=1):
+    # Conv2D wrapper, with bias and relu activation
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.bias_add(x, b)
+    return tf.nn.relu(x)
+
+
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.01)
   return tf.Variable(initial)
@@ -18,10 +25,10 @@ def bias_variable(shape):
 
 
 def loglikelihood(mean_arr, sampled_arr, sigma):
-  mu = tf.pack(mean_arr)  # mu = [timesteps, batch_sz, loc_dim]
-  sampled = tf.pack(sampled_arr)  # same shape as mu
+  mu = tf.stack(mean_arr)  # mu = [timesteps, batch_sz, loc_dim]
+  sampled = tf.stack(sampled_arr)  # same shape as mu
   gaussian = distributions.Normal(mu, sigma)
-  logll = gaussian.log_pdf(sampled)  # [timesteps, batch_sz, loc_dim]
+  logll = gaussian.log_prob(sampled)  # [timesteps, batch_sz, loc_dim]
   logll = tf.reduce_sum(logll, 2)
   logll = tf.transpose(logll)  # [batch_sz, timesteps]
   return logll

c992af4437d53110e3f7212be26b95a0832292e8
Ethan Petersen <ethanpetersen@ethans-mbp.wlan.rose-hulman.edu>
2017-06-29 11:22:28 -0400

    Specified one gpu for DRAM training.

--- DRAM/dram.py
+++ DRAM/dram.py
@@ -30,130 +30,130 @@ n_steps = config.step
 loc_mean_arr = []
 sampled_loc_arr = []
 
+with tf.device('/gpu:0'):
+  def get_next_input(output, i):
+    global context
+    second_lstm_output, context = second_lstm(output, context)
+    loc, loc_mean = loc_net(second_lstm_output)
+    gl_next = gl(loc)
+    loc_mean_arr.append(loc_mean)
+    sampled_loc_arr.append(loc)
+    return gl_next
 
-def get_next_input(output, i):
-  global context
-  second_lstm_output, context = second_lstm(output, context)
-  loc, loc_mean = loc_net(second_lstm_output)
-  gl_next = gl(loc)
-  loc_mean_arr.append(loc_mean)
-  sampled_loc_arr.append(loc)
-  return gl_next
+  # placeholders
+  images_ph = tf.placeholder(tf.float32,
+                             [None,
+                             config.original_size,
+                             config.original_size,
+                             config.num_channels])
+  labels_ph = tf.placeholder(tf.int64, [None])
 
-# placeholders
-images_ph = tf.placeholder(tf.float32,
-                           [None,
-                           config.original_size,
-                           config.original_size,
-                           config.num_channels])
-labels_ph = tf.placeholder(tf.int64, [None])
+  # Build the aux nets.
+  with tf.variable_scope('glimpse_net'):
+    gl = GlimpseNet(config, images_ph)
+  with tf.variable_scope('loc_net'):
+    loc_net = LocNet(config)
+  with tf.variable_scope('context_net'):
+    cl = ContextNet(config)
 
-# Build the aux nets.
-with tf.variable_scope('glimpse_net'):
-  gl = GlimpseNet(config, images_ph)
-with tf.variable_scope('loc_net'):
-  loc_net = LocNet(config)
-with tf.variable_scope('context_net'):
-  cl = ContextNet(config)
+  # number of examples
+  N = tf.shape(images_ph)[0]
 
-# number of examples
-N = tf.shape(images_ph)[0]
-
-# Process context
-context = cl(images_ph)
-loc = loc_net(context)[0]
-glimpse = gl(loc)
-
-# Core network.
-with tf.variable_scope('first', reuse=None):
-  first_lstm = tf.contrib.rnn.BasicLSTMCell(
-            config.cell_size, forget_bias=0.0, state_is_tuple=False,
-            reuse=tf.get_variable_scope().reuse)
-with tf.variable_scope('second', reuse=None):
-  second_lstm = tf.contrib.rnn.BasicLSTMCell(
-            config.cell_size, forget_bias=0.0, state_is_tuple=False,
-            reuse=tf.get_variable_scope().reuse)
-first_state = tf.zeros((N, 2*config.cell_size))
-second_state = tf.concat([context, context], axis=1)
-
-outputs = []
-for time_step in range(config.num_glimpses):
-  with tf.variable_scope('first'):
-    if time_step > 0: tf.get_variable_scope().reuse_variables()
-    first_output, first_state = first_lstm(glimpse, first_state)
-  with tf.variable_scope('second'):
-    if time_step > 0: tf.get_variable_scope().reuse_variables()
-    second_output, second_state = second_lstm(first_output, second_state)
-  loc, loc_mean = loc_net(second_output)
+  # Process context
+  context = cl(images_ph)
+  loc = loc_net(context)[0]
   glimpse = gl(loc)
-  loc_mean_arr.append(loc_mean)
-  sampled_loc_arr.append(loc)
-  outputs.append(first_output)
 
-# inputs = [init_glimpse]
-# inputs.extend([0] * (config.num_glimpses))
-# outputs, _ = seq2seq.rnn_decoder(
-#     decoder_inputs=inputs,
-#     initial_state=context,
-#     cell=first_lstm,
-#     loop_function=get_next_input)
+  # Core network.
+  with tf.variable_scope('first', reuse=None):
+    first_lstm = tf.contrib.rnn.BasicLSTMCell(
+              config.cell_size, forget_bias=0.0, state_is_tuple=False,
+              reuse=tf.get_variable_scope().reuse)
+  with tf.variable_scope('second', reuse=None):
+    second_lstm = tf.contrib.rnn.BasicLSTMCell(
+              config.cell_size, forget_bias=0.0, state_is_tuple=False,
+              reuse=tf.get_variable_scope().reuse)
+  first_state = tf.zeros((N, 2*config.cell_size))
+  second_state = tf.concat([context, context], axis=1)
 
-# Time independent baselines
-with tf.variable_scope('baseline'):
-  w_baseline = weight_variable((config.cell_output_size, 1))
-  b_baseline = bias_variable((1,))
-baselines = []
-for t, output in enumerate(outputs):
-  baseline_t = tf.nn.xw_plus_b(output, w_baseline, b_baseline)
-  baseline_t = tf.squeeze(baseline_t)
-  baselines.append(baseline_t)
-baselines = tf.stack(baselines)  # [timesteps, batch_sz]
-baselines = tf.transpose(baselines)  # [batch_sz, timesteps]
+  outputs = []
+  for time_step in range(config.num_glimpses):
+    with tf.variable_scope('first'):
+      if time_step > 0: tf.get_variable_scope().reuse_variables()
+      first_output, first_state = first_lstm(glimpse, first_state)
+    with tf.variable_scope('second'):
+      if time_step > 0: tf.get_variable_scope().reuse_variables()
+      second_output, second_state = second_lstm(first_output, second_state)
+    loc, loc_mean = loc_net(second_output)
+    glimpse = gl(loc)
+    loc_mean_arr.append(loc_mean)
+    sampled_loc_arr.append(loc)
+    outputs.append(first_output)
 
-# Take the last step only.
-output = outputs[-1]
-# Build classification network.
-with tf.variable_scope('cls'):
-  w_logit = weight_variable((config.cell_output_size, config.num_classes))
-  b_logit = bias_variable((config.num_classes,))
-logits = tf.nn.xw_plus_b(output, w_logit, b_logit)
-softmax = tf.nn.softmax(logits)
+  # inputs = [init_glimpse]
+  # inputs.extend([0] * (config.num_glimpses))
+  # outputs, _ = seq2seq.rnn_decoder(
+  #     decoder_inputs=inputs,
+  #     initial_state=context,
+  #     cell=first_lstm,
+  #     loop_function=get_next_input)
 
-# cross-entropy.
-xent = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels_ph)
-xent = tf.reduce_mean(xent)
-pred_labels = tf.argmax(logits, 1)
-# 0/1 reward.
-reward = tf.cast(tf.equal(pred_labels, labels_ph), tf.float32)
-rewards = tf.expand_dims(reward, 1)  # [batch_sz, 1]
-rewards = tf.tile(rewards, (1, config.num_glimpses))  # [batch_sz, timesteps]
-logll = loglikelihood(loc_mean_arr, sampled_loc_arr, config.loc_std)
-advs = rewards - tf.stop_gradient(baselines)
-logllratio = tf.reduce_mean(logll * advs)
-reward = tf.reduce_mean(reward)
+  # Time independent baselines
+  with tf.variable_scope('baseline'):
+    w_baseline = weight_variable((config.cell_output_size, 1))
+    b_baseline = bias_variable((1,))
+  baselines = []
+  for t, output in enumerate(outputs):
+    baseline_t = tf.nn.xw_plus_b(output, w_baseline, b_baseline)
+    baseline_t = tf.squeeze(baseline_t)
+    baselines.append(baseline_t)
+  baselines = tf.stack(baselines)  # [timesteps, batch_sz]
+  baselines = tf.transpose(baselines)  # [batch_sz, timesteps]
 
-baselines_mse = tf.reduce_mean(tf.square((rewards - baselines)))
-var_list = tf.trainable_variables()
-# hybrid loss
-loss = -logllratio + xent + baselines_mse  # `-` for minimize
-grads = tf.gradients(loss, var_list)
-grads, _ = tf.clip_by_global_norm(grads, config.max_grad_norm)
+  # Take the last step only.
+  output = outputs[-1]
+  # Build classification network.
+  with tf.variable_scope('cls'):
+    w_logit = weight_variable((config.cell_output_size, config.num_classes))
+    b_logit = bias_variable((config.num_classes,))
+  logits = tf.nn.xw_plus_b(output, w_logit, b_logit)
+  softmax = tf.nn.softmax(logits)
 
-# learning rate
-global_step = tf.get_variable(
-    'global_step', [], initializer=tf.constant_initializer(0), trainable=False)
-training_steps_per_epoch = mnist.train.num_examples // config.batch_size
-starter_learning_rate = config.lr_start
-# decay per training epoch
-learning_rate = tf.train.exponential_decay(
-    starter_learning_rate,
-    global_step,
-    training_steps_per_epoch,
-    0.97,
-    staircase=True)
-learning_rate = tf.maximum(learning_rate, config.lr_min)
-opt = tf.train.AdamOptimizer(learning_rate)
-train_op = opt.apply_gradients(zip(grads, var_list), global_step=global_step)
+  # cross-entropy.
+  xent = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels_ph)
+  xent = tf.reduce_mean(xent)
+  pred_labels = tf.argmax(logits, 1)
+  # 0/1 reward.
+  reward = tf.cast(tf.equal(pred_labels, labels_ph), tf.float32)
+  rewards = tf.expand_dims(reward, 1)  # [batch_sz, 1]
+  rewards = tf.tile(rewards, (1, config.num_glimpses))  # [batch_sz, timesteps]
+  logll = loglikelihood(loc_mean_arr, sampled_loc_arr, config.loc_std)
+  advs = rewards - tf.stop_gradient(baselines)
+  logllratio = tf.reduce_mean(logll * advs)
+  reward = tf.reduce_mean(reward)
+
+  baselines_mse = tf.reduce_mean(tf.square((rewards - baselines)))
+  var_list = tf.trainable_variables()
+  # hybrid loss
+  loss = -logllratio + xent + baselines_mse  # `-` for minimize
+  grads = tf.gradients(loss, var_list)
+  grads, _ = tf.clip_by_global_norm(grads, config.max_grad_norm)
+
+  # learning rate
+  global_step = tf.get_variable(
+      'global_step', [], initializer=tf.constant_initializer(0), trainable=False)
+  training_steps_per_epoch = mnist.train.num_examples // config.batch_size
+  starter_learning_rate = config.lr_start
+  # decay per training epoch
+  learning_rate = tf.train.exponential_decay(
+      starter_learning_rate,
+      global_step,
+      training_steps_per_epoch,
+      0.97,
+      staircase=True)
+  learning_rate = tf.maximum(learning_rate, config.lr_min)
+  opt = tf.train.AdamOptimizer(learning_rate)
+  train_op = opt.apply_gradients(zip(grads, var_list), global_step=global_step)
 
 with tf.Session() as sess:
   sess.run(tf.initialize_all_variables())

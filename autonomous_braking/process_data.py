8fbeb96ada74035c7013a5dab1c0658408cc5af6
Ethan Petersen <ethanpetersen@ethans-mbp.wlan.rose-hulman.edu>
2017-07-18 21:04:36 -0400

    Rewrote process data to create gaze buckets.

--- autonomous_braking/process_data.py
+++ autonomous_braking/process_data.py
@@ -6,6 +6,19 @@ import pandas as pd
 from tqdm import tqdm
 from random import shuffle
 
+# Bucketing parameters
+num_buckets = 4
+
+def get_bucket(num_buckets, x, y, width, height):
+    delta_width = width / num_buckets
+    delta_height = height / num_buckets
+
+    for i in range(num_buckets):
+        if x < i * delta_width:
+            for j in range(num_buckets):
+                if y < j * delta_height:
+                    return (num_buckets * j + i)
+
 # Columns: Frame, Brake, GazeX, GazeY
 dataFile = './data/cleaned_data.csv'
 df = pd.read_csv(dataFile, delimiter='\t')
@@ -23,10 +36,8 @@ filename = 'data/driving.avi'
 vid = imageio.get_reader(filename,  'ffmpeg')
 batch = 0
 count = 0
-left_imgs = np.zeros((1000, 244, 450, 3))
-right_imgs = np.zeros((1000, 244, 450, 3))
-center_imgs = np.zeros((1000, 244, 450, 3))
-gazes = np.zeros((1000, 2))
+buckets = np.zeros((1000, 16))
+left_imgs = np.zeros((1000, 244, 900, 3))
 braking = np.zeros((1000, 2))
 img_size = (244, 900, 3)
 print("Processing frames...")
@@ -36,42 +47,29 @@ for index in indices:
     frame = df.loc[index, 'Frame']
     x = df.loc[index, 'GazeX']
     y = df.loc[index, 'GazeY']
-    brake = df.loc[index, 'Brake']
     
     image = vid.get_data(frame)
     if image.shape == img_size:
         height, width, depth = image.shape
-        left_img = image[:, :width/2, :]
-        right_img = image[:, width/2:, :]
-        center_img = image[:, (width/2 - width/4): (width/2 + width/4), :]
 
         # Store images
-        left_imgs[count] = left_img
-        right_imgs[count] = right_img
-        center_imgs[count] = center_img
+        imgs[count] = image
+        buckets[count] = get_bucket(num_buckets, x, y, 244, 900)
 
         # Store gaze location
         gazes[count] = np.array([x, y])
-        
-        # Store braking info
-        if brake == 0:
-            braking[count] = np.array([1, 0])  # nonbraking on left
-        else:
-            braking[count] = np.array([0, 1])  # braking on right
 
         # increment count
         count += 1
         # Wipe and save batch
         if count % 1000 == 0:
             print("processed: {0}".format(count))
-            SAVE_FILE_NAME = '/home/data2/vision6/ethpete/ab_data/batch_{0}'.format(batch)
+            SAVE_FILE_NAME = '/home/data2/vision6/ethpete/gaze_data/batch_{0}'.format(batch)
             np.savez_compressed(
                 SAVE_FILE_NAME,
-                left_imgs=left_imgs,
-                right_imgs=right_imgs,
-                center_imgs=center_imgs,
+                imgs=left_imgs,
                 gazes=gazes,
-                braking=braking)
+                buckets=buckets)
             print("Saved " + SAVE_FILE_NAME)
 
             # Reset
